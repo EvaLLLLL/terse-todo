307490746d17071fe7d03295d736a5ade1b9cd7a
lyhhh <17610603221@163.com>
2020-10-14 19:29:30 +0800

    first commit

---
+++ cli.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const program = require('commander')
+const api = require('./index.js')
+
+program
+	.option('-x, --xxx', 'what is the x')
+program
+	.command('add')
+	.description('add a task')
+	.action((...args) => {
+		let words = args.slice(0, -1)
+		api.add(words).then(() => {
+			console.log('添加成功')
+		}, () => {
+			console.log('添加失败')
+		})
+	})
+program
+	.command('clear')
+	.description('clear all tasks')
+	.action(() => {
+		api.clear().then(() => {
+			console.log('清除完毕')
+		}, () => {
+			console.log('清除失败')
+		})
+	})
+
+program.parse(process.argv)
+
+if (process.argv.length === 2) {
+	void api.showAll()
+}
